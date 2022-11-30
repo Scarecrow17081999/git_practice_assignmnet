d819a914209b6cda21fcb36611e0be8c9a41108e
Parikshit Kumar <abha.bharti35@gmail.com>
2022-11-30 13:05:55 +0530

    added prime and palindrome problem

---
+++ palindrome_problem.js
@@ -0,0 +1,8 @@
+  
+let str="acbfgjs"
+  
+let x=str1.split(" ").reverse().join(",");
+    
+    
+  a==str1?console.log("Yes"):
+  console.log("No")
---
+++ prime_problem.js
@@ -0,0 +1,11 @@
+let nuum=15;
+
+count=0
+    for(var x=1;x<num;x++){
+        if(num%x==0){
+            count++
+        }
+    }
+    
+    count==2?console.log("Yes")
+    :console.log("No")
